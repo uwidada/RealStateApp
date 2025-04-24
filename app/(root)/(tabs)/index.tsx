650c435cd551ae28055259cb49868e2599585b4a
unknown <dalisauwimana1973@gmail.com>
2025-04-24 17:32:06 +0200

    app setting

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,23 @@
+import { Link } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-lg my-10 text-red-500">WELCOME TO OUR REAL ESTATE APP</Text>
+      <Link href="/sign-in">
+        <Text className="text-blue-600">Sign-In</Text>
+      </Link>
+      <Link href="/explore"> Explore </Link>
+      <Link href="/profile"> Profile </Link>
+      <Link href="/properties/1"> Property </Link>
+    </View>
+  );
+}
